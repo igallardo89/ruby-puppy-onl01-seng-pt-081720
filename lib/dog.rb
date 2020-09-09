ace3cf3e55a6df92b3c61ff7e79f72a7910cee43
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-09 19:35:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,7 @@ class Dog
       @name = name
       @@all << self
     end
-      def self.all
-        @@all.each do |dog|
-         puts dog.name
-      end
+    
     end 
       def Dog.clear_all
         @@all.clear
